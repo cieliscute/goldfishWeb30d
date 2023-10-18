14a9ae50562f6c9819b6ada3163d5f4cbf6a0d09
ciel <cielchang0324@gmail.com>
2023-10-18 18:43:57 +0800

    till day2

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/goldfishweb30d/",
+  base: "/goldfishWeb30d/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
